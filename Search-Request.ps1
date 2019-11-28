609660d19aa8ffae0420d706365c78ab0a12b1f1
pablogoofy2000 <pablogoofy2000@gmail.com>
2019-11-28 00:26:18 +0000

    Search by subject

--- Search-Request.ps1
+++ Search-Request.ps1
@@ -1,8 +1,33 @@
 
-$ApiKey = "D89B534B-A2CA-4E31-A992-0D23C472EA9E" # Technician API Key
+$ApiKey = "FE305333-9BBB-40FD-A93F-6E5719586C54" # Technician API Key
 $SdpUri = "https://demo.servicedeskplus.com" # SDP URL
 $header = @{TECHNICIAN_KEY=$ApiKey}
-$filter = "Open_System"
+function Get-Request
+{
+[CmdletBinding()]
+param
+(
+[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
+[alias ("id")]
+[Int32]
+$RequestID,
+[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+[String]
+$Manager, # Manager field
+[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+[String]
+$RequestFor # Person Request is For
+)
+$input= @"
+{}
+"@
+#$params = @{input_data=$input;format='json'}
+$Uri = $SdpUri + "/api/v3/requests/" + $RequestID
+$result = Invoke-RestMethod -Method Get -Uri $Uri -Headers $header #-OutFile "C:\Users\pablo\source\Power1\Ticket.json"
+$test = $result.request
+$test
+#$request.psobject.TypeNames.Insert(0, "Request")
+}
 function Search-Request
 {
 [CmdletBinding()]
@@ -14,50 +39,47 @@ param
 $RequestID,
 [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
 [String]
-$Manager, # Manager field
-[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
-[String]
-$RequestFor, # Person Request is For
-[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
-[String]
 $subject,
 [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
 [String]
 $priority,
 [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
 [String]
+$Group,
+[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+[String]
 $status
 )
-$filter_by = @{
-        name = $filter
-    }
-$filterby
-$subject = $null
-$priority = "high"
+$filterby = @{
+    name = "open_system"
+}
+#$search =search_fields =@{}
 $input = @{
     list_info = @{
-        "row_count"= "20"
+        "row_count"= "500"
         "start_index"= "1"
         "sort_field"= "subject"
         "sort_order"= "asc"
         "get_total_count"= $true
-        search_fields =@{
-            "subject" = $subject
-            "priority.name" = $priority
-        }
-        filter_by = $filter_by
+        search_fields =@{}
 
     }
 }
-
+if($status -eq "open") {
+    $input.list_info.Add("filter_by", $filterby)
+}
+if(!$subject -eq "") {
+    $input.list_info.search_fields.add("subject", $subject)
+}
 
 $inputJson = $input| ConvertTo-Json -Depth 10
-$inputJson
+#$inputJson
 $params = @{input_data=$inputJson;format='json'}
-$params
+#$params
 $Uri = $SdpUri + "/api/v3/requests"
 $result = Invoke-RestMethod -Method Get -Uri $Uri -Headers $header -Body $params #-OutFile "C:\Users\pablo\source\Power1\Ticket.json"
+#$result
 $results = $result.requests
-$results | foreach { $_.subject}
+$results# | foreach {"RequestID:" + $_.id + " ",  "Subject" + $_.subject, "Requester " + $_.Requester.name}
 }
-Search-Request -subject mails
+Search-Request -subject mainte
